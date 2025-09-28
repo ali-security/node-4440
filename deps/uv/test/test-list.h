d706926d7b6b97e700fe0b0b1bd111f1c8183e7b
Tal Samucha <tal.s@sealsecurity.io>
2025-09-28 09:26:04 +0300

    CVE-2021-22918

--- deps/uv/test/test-list.h
+++ deps/uv/test/test-list.h
@@ -493,6 +493,7 @@ TEST_DECLARE  (fork_threadpool_queue_work_simple)
 
 TEST_DECLARE  (idna_toascii)
 TEST_DECLARE  (utf8_decode1)
+TEST_DECLARE  (utf8_decode1_overrun)
 TEST_DECLARE  (uname)
 
 TASK_LIST_START
@@ -1049,6 +1050,7 @@ TASK_LIST_START
 #endif
 
   TEST_ENTRY  (utf8_decode1)
+  TEST_ENTRY  (utf8_decode1_overrun)
   TEST_ENTRY  (uname)
 
 /* Doesn't work on z/OS because that platform uses EBCDIC, not ASCII. */
