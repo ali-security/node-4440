8895863e075e20f3b800747153a24529a26fa4d8
Tal Samucha <tal.s@sealsecurity.io>
2025-09-28 13:29:36 +0300

    revert CVE-2024-27983

--- src/node_http2.cc
+++ src/node_http2.cc
@@ -657,16 +657,11 @@ Http2Session::Http2Session(Environment* env,
 Http2Session::~Http2Session() {
   CHECK_EQ(flags_ & SESSION_STATE_HAS_SCOPE, 0);
   Debug(this, "freeing nghttp2 session");
-  // Ensure that all `Http2Stream` instances and the memory they hold
-  // on to are destroyed before the nghttp2 session is.
-  for (const auto& [id, stream] : streams_) {
-    stream->Destroy();
-  }
-  streams_.clear();
-  // Explicitly reset session_ so the subsequent
-  // current_nghttp2_memory_ check passes.
-  session_.reset();
+  for (const auto& stream : streams_)
+    stream.second->session_ = nullptr;
+  nghttp2_session_del(session_);
   CHECK_EQ(current_nghttp2_memory_, 0);
+  free(stream_buf_allocation_.base);
 }
 
 std::string Http2Session::diagnostic_name() const {
