a72bb11d8edf2315b4b45c1d2297eac79c0eebf2
Tal Samucha <tal.s@sealsecurity.io>
2025-09-28 21:20:25 +0300

    CVE-2024-24806

--- deps/uv/src/idna.c
+++ deps/uv/src/idna.c
@@ -272,6 +272,9 @@ long uv__idna_toascii(const char* s, const char* se, char* d, char* de) {
   char* ds;
   int rc;
 
+  if (s == se)
+    return UV_EINVAL;
+
   ds = d;
 
   si = s;
@@ -306,8 +309,9 @@ long uv__idna_toascii(const char* s, const char* se, char* d, char* de) {
       return rc;
   }
 
-  if (d < de)
-    *d++ = '\0';
+  if (d >= de)
+    return UV_EINVAL;
 
+  *d++ = '\0';
   return d - ds;  /* Number of bytes written. */
 }
